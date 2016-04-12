003eaa9ae2c5eb3c851fc2fdb740047823312ebe
JoseLuisPulido <jlpl.tec@gmail.com>
2016-04-12 10:59:30 -0600

    archivo Js, funcion anonima y consola

---
+++ javascript/js/codigo.js
@@ -0,0 +1,10 @@
+var botonclic = function()
+{
+	alert("Clic del boton");
+}
+var nombre = "José.";
+/*DOM modelo de objetos del documento*/
+document.write("<Strong>JS en acción</Strong>"+" "
+				+nombre);/*document se ejecuta en body por default*/
+console.log("A punto de lanzar un mensaje")
+alert("Mensaje por pantalla");/*es una pantalla modal, no me permite hacer nada hasta cerrarla*/
