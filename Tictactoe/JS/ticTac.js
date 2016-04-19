472c6f875c27d0df883377b63b063ef4ce2dd62c
JoseLuisPulido <jlpl.tec@gmail.com>
2016-04-19 11:05:32 -0600

    gato escribe Xs

---
+++ Tictactoe/JS/ticTac.js
@@ -0,0 +1,66 @@
+var contClicks = 0;
+
+function writethings1() {
+	var check1 = document.getElementById('td1').value;
+	if(check1 == null) {
+		document.getElementById('td1').innerHTML = 'X';
+		contClicks++;
+	}
+
+}
+function writethings2() {
+	var check2 = document.getElementById('td2').value;
+	if(check2 == null) {
+		document.getElementById('td2').innerHTML = 'X';
+		contClicks++;
+	}
+}
+function writethings3() {
+	var check3 = document.getElementById('td3').value;
+	if(check3 == null) {
+		document.getElementById('td3').innerHTML = 'X';
+		contClicks++;
+	}
+}
+function writethings4() {
+	var check4 = document.getElementById('td4').value;
+	if(check4 == null) {
+		document.getElementById('td4').innerHTML = 'X';
+		contClicks++;
+	}
+}
+function writethings5() {
+	var check5 = document.getElementById('td5').value;
+	if(check5 == null) {
+		document.getElementById('td5').innerHTML = 'X';
+		contClicks++;
+	}
+
+}	function writethings6() {
+	var check6 = document.getElementById('td6').value;
+	if(check6 == null) {
+		document.getElementById('td6').innerHTML = 'X';
+		contClicks++;
+	}
+}
+	function writethings7() {
+	var check7 = document.getElementById('td7').value;
+	if(check7 == null) {
+		document.getElementById('td7').innerHTML = 'X';
+		contClicks++;
+	}
+}
+	function writethings8() {
+	var check8 = document.getElementById('td8').value;
+	if(check8 == null) {
+		document.getElementById('td8').innerHTML = 'X';
+		contClicks++;
+	}
+}
+	function writethings9() {
+	var check9 = document.getElementById('td9').value;
+	if(check9 == null) {
+		document.getElementById('td9').innerHTML = 'X';
+		contClicks++;
+	}
+}
