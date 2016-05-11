3e3c8ee4c92d5a702efcbcf44b125cde15892b36
JoseLuisPulido <jlpl.tec@gmail.com>
2016-05-11 10:50:27 -0600

    login funcionando

--- jquery2/js/eventos.js
+++ jquery2/js/eventos.js
@@ -2,16 +2,32 @@ var iniciaApp = function()
 {
 	var validarEntrada = function()
 	{
-		var usuario = $("#txtUsuario".val())
-		var clave   = $("#txtClave".val())
+		//invalida los eventos que no corresponden a esta funcion
+		event.preventDefault();
+		var usuario = $("#txtUsuario").val();
+		var clave   = $("#txtClave").val();
 		//validaciones
 		//1.- que no acete vacios
 		if (usuario == "")
 		{
 			alert("El usuario no debe ser vacio");
-			$("txtUsuario").focus();
+			$("#txtUsuario").focus();
 		}
-		console.log("Se disparó el submit")
+		if(clave == "")
+		{
+			alert("La contraseña no debe ser vacia");
+			$("#txtClave").focus();
+		}
+		//2.- Verificar usuario y contraseña
+		if(usuario == "pw" && clave == "1234")
+		{
+			//Dar entrada al usuario
+			$("#datosUsuario").hide(); //escondemos
+			$("nav").show();
+		}
+		else
+			alert("usuario y/o contraseña incorrecta");
+		console.log("Se disparó el submit");
 	}
 	$("#frmValidaEntrada").on("submit",validarEntrada);
 }
